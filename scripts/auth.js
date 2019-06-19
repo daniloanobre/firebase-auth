b99d681677c99ec897bca0d725bf6615d76284b6
Danilo <silva.danilo.nobre@gmail.com>
2019-06-18 21:37:49 -0300

    Create guide

--- scripts/auth.js
+++ scripts/auth.js
@@ -11,6 +11,26 @@ auth.onAuthStateChanged(user => {
     setupGuides([]);
   }
 });
+
+// create new guide
+const createGuideForm = document.querySelector("#create-form");
+createGuideForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const title = createGuideForm["title"].value;
+  const content = createGuideForm["content"].value;
+
+  db.collection("guides")
+    .add({ title, content })
+    .then(ref => {
+      // close the create guide modal and reset the form
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    })
+    .catch(err => console.log(err.message));
+});
+
 // sign up
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", e => {
